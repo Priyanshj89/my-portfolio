c8da072de9d7c597ce577820582ca245da88c391
Priyansh.j89 <55475866+Priyanshj89@users.noreply.github.com>
2020-12-21 19:35:49 +0530

    Minor Changes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -20,7 +20,7 @@ function Footer() {
 						</a>
 					</div>
 				</div>
-				<hr style={{ backgroundColor: 'white' }}></hr>
+				<hr style={{ backgroundColor: 'white' }} className="seperator"></hr>
 				<div className="mv1">
 					<div className="social">Contact Details</div>
 					<div className="contact">Phone No. - 9167973298</div>
